7565baf295226376cd7bc01d383e13f4dc781967
Subin Min <tnqls5417@naver.com>
2022-11-07 19:25:28 +0900

    feat: 필요 커스텀 함수 작성

--- src/main/java/oncoding/concoder/repository/ProblemCategoryRepository.java
+++ src/main/java/oncoding/concoder/repository/ProblemCategoryRepository.java
@@ -1,9 +1,11 @@
 package oncoding.concoder.repository;
 
+import java.util.List;
 import java.util.UUID;
 import oncoding.concoder.model.ProblemCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProblemCategoryRepository extends JpaRepository<ProblemCategory, UUID> {
+    List<ProblemCategory> findByCategoryId(UUID id);
 
 }
--- src/main/java/oncoding/concoder/repository/ProblemRepository.java
+++ src/main/java/oncoding/concoder/repository/ProblemRepository.java
@@ -1,9 +1,12 @@
 package oncoding.concoder.repository;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 import oncoding.concoder.model.Problem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProblemRepository extends JpaRepository<Problem, UUID> {
-
+    Optional<Problem> findFirstByNumber(int number);
+    List<Problem> findByLevelId(UUID id);
 }
