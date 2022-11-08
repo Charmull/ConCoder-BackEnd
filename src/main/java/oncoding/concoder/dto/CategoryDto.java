252440968ffe307e4bf51c1919f6eedbd5a26189
Subin Min <tnqls5417@naver.com>
2022-11-08 22:18:13 +0900

    fix: Dto에 setter 설정 추가
    - Mapper 통해서 Dto로의 변환 위해서 Response Dto여도 setter 설정 필요

--- src/main/java/oncoding/concoder/dto/CategoryDto.java
+++ src/main/java/oncoding/concoder/dto/CategoryDto.java
@@ -2,10 +2,12 @@ package oncoding.concoder.dto;
 
 import java.util.UUID;
 import lombok.Getter;
+import lombok.Setter;
 import lombok.ToString;
 
 public class CategoryDto {
     @Getter
+    @Setter
     @ToString
     public static class Response {
         private UUID id;
--- src/main/java/oncoding/concoder/dto/LevelDto.java
+++ src/main/java/oncoding/concoder/dto/LevelDto.java
@@ -1,9 +1,14 @@
 package oncoding.concoder.dto;
 
 import java.util.UUID;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
 public class LevelDto {
-
+    @Getter
+    @Setter
+    @ToString
     public static class Response {
         private UUID id;
         private String name;
