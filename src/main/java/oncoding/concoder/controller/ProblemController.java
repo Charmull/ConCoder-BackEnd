20ae77ce181372dc224735940ea95f633defbe1e
Subin Min <tnqls5417@naver.com>
2022-11-12 21:40:10 +0900

    fix: Content Type 명시

--- src/main/java/oncoding/concoder/controller/ProblemController.java
+++ src/main/java/oncoding/concoder/controller/ProblemController.java
@@ -12,13 +12,14 @@ import oncoding.concoder.mapper.ProblemDtoMapper;
 import oncoding.concoder.service.CategoryService;
 import oncoding.concoder.service.LevelService;
 import oncoding.concoder.service.ProblemService;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/problems")
+@RequestMapping(value = "/problems", produces = MediaType.APPLICATION_JSON_VALUE)
 @RequiredArgsConstructor
 public class ProblemController {
     private final ProblemService problemService;
