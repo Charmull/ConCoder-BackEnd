8419979a2cd2e38d54e7632150fbcbd9ee4c0843
신정아 <jeonga@khu.ac.kr>
2022-11-17 17:28:13 +0900

    chore : websocsketconfig 설정
    https://github.com/KHUCapston-concoder/Backend/issues/15

--- src/main/java/oncoding/concoder/config/WebSocketConfig.java
+++ src/main/java/oncoding/concoder/config/WebSocketConfig.java
@@ -1,36 +1,39 @@
 package oncoding.concoder.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.EventListener;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 
 @Configuration
-@EnableWebSocketMessageBroker
-public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+@EnableWebSocketMessageBroker //웹 소켓 메시지 처리 활성화
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer{
 
-  /**
-   * 메모리 기반의 Simple Message Broker를 활성화
-   * 메시지 브로커는 "/subscribe"으로 시작하는 주소의 Subscriber들에게 메시지를 전달하는 역할
-   * @param registry
-   */
   @Override
-  public void configureMessageBroker(MessageBrokerRegistry registry) {
-    registry.enableSimpleBroker("/subscribe");
-    registry.setApplicationDestinationPrefixes("/publish"); //클라이언트가 서버로 메시지 보낼 때 붙여야 하는 prefix를 지정
+  public void configureMessageBroker(final MessageBrokerRegistry registry) {
+    // 메세지 브로커는 아래 주소로 시작하는 주소의 Subscriber 에게 메시지를 전달하는 역할을 한다.
+    // 일반적으로 /topic은 한 명이 메시지를 발행했을 때, 해당 토픽을 구독하고 있는 n명에게 메시지를 뿌릴 때 사용
+    // 반면에 /queue는 한 명이 메시지를 발행했을 때, 발행한 한 명에게 다시 정보를 보내는 경우
+    registry.enableSimpleBroker("/sub", "/subscribe", "/queue", "/topic");
+    // 클라이언트가 서버로 메시지를 보낼 때 아래 주소를 붙인다.
+    //즉 도착 경로에 대한 prefix 설정
+    //예를 들어 /app으로 설정해두면 /topic/hello라는 토픽에 대해 구독을 신청했을 때, 실제 경로는 /app/topic/hello가 되는 것
+    registry.setApplicationDestinationPrefixes("/pub", "/publish", "/app");
   }
 
-  /**
-   * HandShake와 통신을 담당할 EndPoint를 지정
-   * 클라이언트에서 서버로 WebSocket 연결을 하고 싶을 때, "/ws-connection"으로 요청을 보내도록
-   * @param registry
-   */
   @Override
-  public void registerStompEndpoints(StompEndpointRegistry registry) {
-    registry.addEndpoint("/ws-connection")
-        .setAllowedOrigins("chrome-extension://ggnhohnkfcpcanfekomdkjffnfcjnjam")
-        .withSockJS();
+  public void registerStompEndpoints(final StompEndpointRegistry registry) {
+    registry.addEndpoint("/ws-connection") // 클라이언트가 웹 소켓 요청을 하고 싶을 때 해당 End point 로 요청을 보낸다.
+        .setAllowedOrigins("*")
+        .withSockJS(); // fallback
   }
+
+  @EventListener
+  public void onDisconnectEvent(final SessionDisconnectEvent event) {
+    System.out.println("hello");
+  }
+
 }
