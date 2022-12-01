47d7b61b9e697e188b6369f07076339b426ebb9c
Jeong <63828202+JeongA-Shin@users.noreply.github.com>
2022-12-01 21:00:07 +0900

    채팅 roomId 자료형 수정 (#67)
    * feat : 방 구독자들에게 메세지 전체 전달하기
    
    https://github.com/KHUCapston-concoder/Backend/issues/58
    
    * fix : 확인 로그 추가
    
    https://github.com/KHUCapston-concoder/Backend/issues/58
    
    * feat : 채팅 컨틀롤러 확인 로그 추가
    
    https://github.com/KHUCapston-concoder/Backend/issues/58
    
    * 자료형 변경

--- src/main/java/oncoding/concoder/controller/ChattingController.java
+++ src/main/java/oncoding/concoder/controller/ChattingController.java
@@ -33,9 +33,10 @@ public class ChattingController {
    * @param request
    */
   @MessageMapping("/rooms/chat/{roomId}")
-  public void chat(@DestinationVariable final UUID roomId, final MessageRequest request) {
+  public void chat(@DestinationVariable final String roomId, final MessageRequest request) {
     log.info("/rooms/chat/"+roomId+" userId:  "+request.getUserId());
     log.info("/rooms/chat/"+roomId+" content: "+request.getContent());
+
     template.convertAndSend("/sub/rooms/chat/"+ roomId , chatService.sendMessage(request));
     log.info("after chatting convert and send");
     template.convertAndSend("/sub/rooms/chat/"+ roomId , chatService.sendMessage(request));
