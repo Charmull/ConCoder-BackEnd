b34a1d218d0110c9a764a31a05727f70c357d646
Charmull <527wlals@gmail.com>
2023-05-31 20:42:46 +0900

    feat: OpenVidu SFU 구조 적용을 위한 API 개발
    - build.gradle에 openvidu 플러그인 추가
    - docker-compose에 openvidu 컨테이너 세팅 추가
    - VideoRoomController에 SFU 구조 적용 API 구현

--- src/main/java/oncoding/concoder/controller/VideoRoomController.java
+++ src/main/java/oncoding/concoder/controller/VideoRoomController.java
@@ -28,6 +28,17 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.socket.messaging.SessionConnectEvent;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+import io.openvidu.java.client.Connection;
+import io.openvidu.java.client.ConnectionProperties;
+import io.openvidu.java.client.OpenVidu;
+import io.openvidu.java.client.OpenViduHttpException;
+import io.openvidu.java.client.OpenViduJavaClientException;
+import io.openvidu.java.client.Session;
+import io.openvidu.java.client.SessionProperties;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PathVariable;
+import javax.annotation.PostConstruct;
 
 @Slf4j
 @RestController
@@ -43,6 +54,15 @@ public class VideoRoomController {
 
   private Map<UUID, Object> usersAtRooms;
 
+  @Value("${OPENVIDU_SECRET}")
+  private String OPENVIDU_SECRET;
+
+  private OpenVidu openvidu;
+
+  @PostConstruct
+  public void init() {
+    this.openvidu = new OpenVidu("http://localhost:4443/", OPENVIDU_SECRET);
+  }
 
 
   @MessageMapping("/video/chat/{roomId}")
@@ -134,7 +154,35 @@ public class VideoRoomController {
 
   }
 
+  /**
+   * @param params The Session properties
+   * @return The Session ID
+   */
+  @PostMapping("/sessions")
+  public ResponseEntity<String> initializeSession(@RequestBody(required = false) Map<String, Object> params)
+          throws OpenViduJavaClientException, OpenViduHttpException {
+    SessionProperties properties = SessionProperties.fromJson(params).build();
+    Session session = openvidu.createSession(properties);
+    return new ResponseEntity<>(session.getSessionId(), HttpStatus.OK);
+  }
 
+  /**
+   * @param sessionId The Session in which to create the Connection
+   * @param params    The Connection properties
+   * @return The Token associated to the Connection
+   */
+  @PostMapping("/sessions/{sessionId}/connections")
+  public ResponseEntity<String> createConnection(@PathVariable("sessionId") String sessionId,
+                                                 @RequestBody(required = false) Map<String, Object> params)
+          throws OpenViduJavaClientException, OpenViduHttpException {
+    Session session = openvidu.getActiveSession(sessionId);
+    if (session == null) {
+      return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+    ConnectionProperties properties = ConnectionProperties.fromJson(params).build();
+    Connection connection = session.createConnection(properties);
+    return new ResponseEntity<>(connection.getToken(), HttpStatus.OK);
+  }
 
 
   // caller의 정보를 다른 callee들에게 쏴준다.
